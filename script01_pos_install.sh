bacb65dc02e3ca182ec4e3901560b80600612136
alvescarvalho <suporte.mateuscarvalho@gmail.com>
2021-02-20 19:38:42 -0300

    Opção 1 & 2 configuradas

--- script01_pos_install.sh
+++ script01_pos_install.sh
@@ -16,7 +16,7 @@
 # ------------------------------------------------------------# 
 # Crescimento e evolução do Script v1.0
 # ------------------------------------------------------------#
-# -------------------------VARIÁVEIS--------------------------#
+# -------------------------ÍNICIO -VARIÁVEIS------------------#
 # ------------------------------------------------------------#
 set -e 
 #PROGRAMAS PARA EXCLUIR 
@@ -84,6 +84,9 @@ app_install=(
 trava1="/var/lib/dpkg/lock-frontend"
 trava2="/var/cache/apt/archives/lock"
 
+#ATUALIZAR SISTEMA
+atualiza_sistema="apt update"
+
 #ADICIONANDO SUPORTE A 32bits
 suporte="dpkg --add-architecture i386"
 
@@ -120,5 +123,56 @@ repositorios=(
 
 # CRIANDO DIRETÓRIO TEMPORÁRIO 
 cd $(mktemp -d)
+# -------------------------FIM -VARIÁVEIS------------------#
+
+
+
 # ------------------------------------------------------------#
 # --------------------ÍNICIO DO SCRIPT------------------------#
+TIME=3
+while true; clear; 
+    do
+    echo " "
+    echo " "
+echo "Seja bem vindo ao script de automação Sr.$USER $0 do Kubuntu!"
+echo ""
+echo "Escolha uma opção abaixo para começar!"
+echo " 
+      1 - Adicionando Arquitetura em 32bits 
+      2 - Desisntalando Programas
+      3 - Adicionando Repositórios
+      4 - Atualizar o Sistema
+      5 - Remover Travas Eventuais do APT
+      6 - Instalando Programas em  APT
+      7 - Instalando Programas em .DEB
+      8 - Instalando Programas em .AppImage
+      9 - Desisntalar Programas desnecessários
+      0 - Sair do sistema"
+echo " "
+echo -n "Digite a opção escolhida:"
+
+read opcao 
+case $opcao in
+     1)        
+         echo "Adicionando suporte para arquitetura em 32bits em."
+         echo
+         sudo ${suporte[@]} 
+         echo "Perfeito adicionado a arquitetura em 32bits."
+         echo 
+         sudo  ${atualiza_sistema[@]}
+         clear
+         echo
+         echo "Perfeito, estamos voltando ao menu iniciar."
+         echo "Reiniciando menu em 3seg..."
+         sleep $TIME 
+
+     ;;
+
+     2)
+        echo "Certo estamos excluindo os programas que você pediu"
+        echo
+        sudo apt purge ${app_remover[@]} -y 
+        
+     ;;              
+esac
+done
