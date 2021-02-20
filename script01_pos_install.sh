85bdae9330145c8c1d08212d57b776902ed9a4e8
alvescarvalho <suporte.mateuscarvalho@gmail.com>
2021-02-20 16:40:55 -0300

    Adicionando Cabeçalho
    Adição dos dados do criador

---
+++ script01_pos_install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e 
+#
+# ---------------------------------------------------#
+# Nome:          posinstall.sh 
+# Descrição:     Pós Install do Kubuntu.
+# Linkedin:      www.linkedin.com/in/""faltacolocar"
+# Escrito por:   Mateus Felipe de Carvalho 
+# Última manutenção feita por:   Mateus Alves 
+# ---------------------------------------------------#
+# Como usar o script:         
+#       $ ./atualiza_sistema.sh
+# ---------------------------------------------------#
+# Bash Version:  
+#              Bash 5.0.17
+# ---------------------------------------------------# 
+# Crescimento e evolução do Script v1.0
+# ---------------------------------------------------#
+# ---------------------------------------------------#
