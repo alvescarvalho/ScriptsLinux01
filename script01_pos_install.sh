dd1024341fb35d56620caced97d459533ebb6fa5
alvescarvalho <suporte.mateuscarvalho@gmail.com>
2021-02-20 16:46:56 -0300

    Adicionando cabeçalho  2

--- script01_pos_install.sh
+++ script01_pos_install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 set -e 
 #
-# ---------------------------------------------------#
-# Nome:          posinstall.sh 
-# Descrição:     Pós Install do Kubuntu.
-# Linkedin:      www.linkedin.com/in/""faltacolocar"
-# Escrito por:   Mateus Felipe de Carvalho 
-# Última manutenção feita por:   Mateus Alves 
-# ---------------------------------------------------#
+# ------------------------------------------------------------#
+# Nome:          script01_pos_install.sh 
+# Descrição:     Automação de pós instalação do Kubntu 20.04.
+# Linkedin:      www.linkedin.com/in/mateusalvescf
+# Escrito por:   Mateus Carvalho 
+# Última edicão feita por: Mateus Carvalho 
+# ------------------------------------------------------------#
 # Como usar o script:         
 #       $ ./atualiza_sistema.sh
-# ---------------------------------------------------#
+# ------------------------------------------------------------#
 # Bash Version:  
 #              Bash 5.0.17
-# ---------------------------------------------------# 
+# ------------------------------------------------------------# 
 # Crescimento e evolução do Script v1.0
-# ---------------------------------------------------#
-# ---------------------------------------------------#
+# ------------------------------------------------------------#
+# ------------------------------------------------------------#
